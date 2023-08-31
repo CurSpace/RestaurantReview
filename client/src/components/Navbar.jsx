b27dfb7db487a24b3058f69eb62901b6db6cc37f
CurSpace <callpradip96@gmail.com>
2023-08-31 00:55:46 -0500

    starting off navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className='navbar bg-dark border-bottom boder-body' data-bs-theme='dark'>
+                <div className='container-fluid'>
+
+                </div>
+            </nav>
+        </>
+    );
+};
+
+export default Navbar;
--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Header from '../components/Header';
 import AddRestaurant from '../components/AddRestaurant';
 import RestaurantList from '../components/RestaurantList';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
   <div>
+        <Navbar/>
         <Header/>
         <AddRestaurant/>
         <RestaurantList/>
